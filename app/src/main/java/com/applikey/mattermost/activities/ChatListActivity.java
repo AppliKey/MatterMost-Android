b3c6451772f85bc2a4f210bd18f87d9b2aef7c35
Mikhail Mikhaylov <nutscracker53@gmail.com>
2016-09-20 20:40:22 +0300

    Remove unnecessary import

--- app/src/main/java/com/applikey/mattermost/activities/ChatListActivity.java
+++ app/src/main/java/com/applikey/mattermost/activities/ChatListActivity.java
@@ -2,7 +2,6 @@ package com.applikey.mattermost.activities;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
