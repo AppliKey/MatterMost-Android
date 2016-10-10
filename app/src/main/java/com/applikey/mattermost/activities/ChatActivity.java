3f7e0ba726ac2c395141f9b0828db6007d39ad56
Mikhail Mikhaylov <nutscracker53@gmail.com>
2016-10-10 16:43:22 +0300

    Introduce ChatActivity

---
+++ app/src/main/java/com/applikey/mattermost/activities/ChatActivity.java
@@ -0,0 +1,25 @@
+package com.applikey.mattermost.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Toolbar;
+
+import com.applikey.mattermost.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class ChatActivity extends BaseMvpActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        setContentView(R.layout.activity_chat);
+
+        ButterKnife.bind(this);
+    }
+}
