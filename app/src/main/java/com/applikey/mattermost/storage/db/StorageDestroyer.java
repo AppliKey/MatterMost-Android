8203d39c74586cb120dfe188e2c6f5f2270d30fb
Shamilov Stanislav <shamilovstas@gmail.com>
2016-11-01 09:07:32 +0200

    add scope

--- app/src/main/java/com/applikey/mattermost/storage/db/StorageDestroyer.java
+++ app/src/main/java/com/applikey/mattermost/storage/db/StorageDestroyer.java
@@ -1,7 +1,10 @@
 package com.applikey.mattermost.storage.db;
 
+import com.applikey.mattermost.injects.PerApp;
+
 import javax.inject.Inject;
 
+@PerApp
 public class StorageDestroyer {
 
     private final Db mDb;
