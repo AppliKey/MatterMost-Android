fb5945a45521b360fd858acafa94b2d6d0bed183
Denis Nikiforov <denis.nikiforov@applikeysolutions.com>
2016-11-11 17:46:02 +0200

    Add comments

--- app/src/main/java/com/applikey/mattermost/mvp/presenters/FavoriteablePresenter.java
+++ app/src/main/java/com/applikey/mattermost/mvp/presenters/FavoriteablePresenter.java
@@ -1,6 +1,9 @@
 package com.applikey.mattermost.mvp.presenters;
 
-
+/**
+ * This is presenter which may respond to "make favorite" command
+ */
+//TODO Fix naming
 public interface FavoriteablePresenter {
 
     void toggleFavorite();
--- app/src/main/java/com/applikey/mattermost/mvp/views/FavoriteableView.java
+++ app/src/main/java/com/applikey/mattermost/mvp/views/FavoriteableView.java
@@ -3,6 +3,10 @@ package com.applikey.mattermost.mvp.views;
 import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
+/**
+ * This is view which may produce to "make favorite" command
+ */
+//TODO Fix naming
 @StateStrategyType(value = SingleStateStrategy.class)
 public interface FavoriteableView {
 
