bcee2e88ae52076876c96a334d3c3b0ac163e14f
tolikprankster <tolikprankster@gmail.com>
2016-11-16 14:58:47 +0200

    search all fixed

--- app/src/main/java/com/applikey/mattermost/mvp/presenters/SearchAllPresenter.java
+++ app/src/main/java/com/applikey/mattermost/mvp/presenters/SearchAllPresenter.java
@@ -1,14 +1,21 @@
 package com.applikey.mattermost.mvp.presenters;
 
+import android.text.TextUtils;
+
 import com.applikey.mattermost.App;
 import com.applikey.mattermost.Constants;
 import com.applikey.mattermost.events.SearchAllTextChanged;
 import com.applikey.mattermost.models.SearchItem;
 import com.applikey.mattermost.models.channel.Channel;
+import com.applikey.mattermost.models.post.Message;
+import com.applikey.mattermost.models.post.Post;
+import com.applikey.mattermost.models.post.PostResponse;
+import com.applikey.mattermost.models.post.PostSearchRequest;
 import com.applikey.mattermost.models.user.User;
 import com.applikey.mattermost.mvp.views.SearchAllView;
 import com.applikey.mattermost.storage.db.ChannelStorage;
 import com.applikey.mattermost.storage.db.UserStorage;
+import com.applikey.mattermost.storage.preferences.Prefs;
 import com.applikey.mattermost.web.ErrorHandler;
 import com.arellomobile.mvp.InjectViewState;
 
@@ -17,12 +24,16 @@ import org.greenrobot.eventbus.Subscribe;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func2;
+import rx.schedulers.Schedulers;
 
 @InjectViewState
 public class SearchAllPresenter extends SearchPresenter<SearchAllView> {
@@ -43,6 +54,9 @@ public class SearchAllPresenter extends SearchPresenter<SearchAllView> {
     String mCurrentUserId;
 
     @Inject
+    Prefs mPrefs;
+
+    @Inject
     ErrorHandler mErrorHandler;
 
     public SearchAllPresenter() {
@@ -57,12 +71,26 @@ public class SearchAllPresenter extends SearchPresenter<SearchAllView> {
     }
 
     public void getData(String text) {
-        if (!mChannelsIsFetched) {
+        if (!mChannelsIsFetched  || TextUtils.isEmpty(text)) {
             return;
         }
         final SearchAllView view = getViewState();
         view.setEmptyState(true);
         mSubscription.clear();
+
+
+        final Observable<List<SearchItem>> postItems =
+                mApi.searchPosts(mPrefs.getCurrentTeamId(), new PostSearchRequest(text))
+                .map(PostResponse::getPosts)
+                        .filter(postMap -> postMap != null)
+                .map(Map::values)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(Observable::from)
+                .flatMap(item -> mChannelStorage.channelById(item.getChannelId()).first(),
+                         (Func2<Post, Channel, SearchItem>) Message::new)
+                .toList();
+
         mSubscription.add(
                 Observable.zip(
                         Channel.getList(mChannelStorage.listUndirected(text))
@@ -80,6 +108,12 @@ public class SearchAllPresenter extends SearchPresenter<SearchAllView> {
 
                             return searchItemList;
                         })
+                        .flatMap(items -> postItems,
+                                 (items, items2) -> {
+                                     items.addAll(items2);
+                                     return items;
+                                 })
+                        .debounce(Constants.INPUT_REQUEST_TIMEOUT_MILLISEC, TimeUnit.MILLISECONDS)
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(view::displayData, mErrorHandler::handleError));
     }
