05d245355426f13405be6137e5563bd11294d23e
Ruslan Sierov <russerua@gmail.com>
2016-11-21 15:16:18 +0200

    SEC -> MIN constant rename

--- app/src/main/java/com/applikey/mattermost/Constants.java
+++ app/src/main/java/com/applikey/mattermost/Constants.java
@@ -30,5 +30,5 @@ public class Constants {
 
     public static final String PERSISTENT_PREFS_FILE_NAME = "PersistentPrefs";
 
-    public static final long SOCKET_SERVICE_SHUTDOWN_THRESHOLD_SEC = 3;
+    public static final long SOCKET_SERVICE_SHUTDOWN_THRESHOLD_MINUTES = 3;
 }
