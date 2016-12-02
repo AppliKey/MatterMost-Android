a6e7824b409423b01cd6c60610331ec732cc5439
tolikprankster <tolikprankster@gmail.com>
2016-12-02 16:52:19 +0200

    fixes after review

--- app/src/main/java/com/applikey/mattermost/utils/SpanUtils.java
+++ app/src/main/java/com/applikey/mattermost/utils/SpanUtils.java
@@ -5,10 +5,6 @@ import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.StyleSpan;
 
-/**
- * @author Anatoliy Chub
- */
-
 public class SpanUtils {
 
     private SpanUtils() {
@@ -18,15 +14,14 @@ public class SpanUtils {
         final int searchLength = search.length();
         final Spannable spannableString = new SpannableString(text);
         int index = text.indexOf(search);
-        if(index < 0) {
+        if (index < 0) {
             return spannableString;
         }
         spannableString.setSpan(new StyleSpan(Typeface.BOLD), index, index + searchLength, 0);
-        while(index >= 0) {
+        while (index >= 0) {
             spannableString.setSpan(new StyleSpan(Typeface.BOLD), index, index + searchLength, 0);
             index = text.indexOf(search, index + 1);
         }
         return spannableString;
-
     }
 }
