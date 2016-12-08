5d426b7c483bd41a79dc26a4833cb605e3175298
tolikprankster <tolikprankster@gmail.com>
2016-12-08 16:52:40 +0200

    Remove sticky headers for one-kind search lists

--- app/src/main/java/com/applikey/mattermost/fragments/SearchAllFragment.java
+++ app/src/main/java/com/applikey/mattermost/fragments/SearchAllFragment.java
@@ -8,6 +8,7 @@ import com.applikey.mattermost.models.SearchItem;
 import com.applikey.mattermost.mvp.presenters.SearchAllPresenter;
 import com.applikey.mattermost.mvp.views.SearchAllView;
 import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersDecoration;
 
 // TODO: Generify search fragments
 public class SearchAllFragment extends SearchFragment implements SearchAllView,
@@ -25,6 +26,12 @@ public class SearchAllFragment extends SearchFragment implements SearchAllView,
         super.onViewCreated(view, savedInstanceState);
         initView(this);
         mPresenter.requestNotJoinedChannels();
+    }
+
+    @Override
+    protected void initView(SearchAdapter.ClickListener clickListener) {
+        super.initView(clickListener);
+        mRecyclerView.addItemDecoration(new StickyRecyclerHeadersDecoration(mAdapter));
     }
 
     @Override
