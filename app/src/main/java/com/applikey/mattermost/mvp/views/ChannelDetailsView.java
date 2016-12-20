9e34b35d084be264322f6664d7b6dc835119e1ee
tolikprankster <tolikprankster@gmail.com>
2016-12-20 14:36:13 +0200

    Invite new member button functionality implemented

--- app/src/main/java/com/applikey/mattermost/mvp/views/ChannelDetailsView.java
+++ app/src/main/java/com/applikey/mattermost/mvp/views/ChannelDetailsView.java
@@ -17,7 +17,7 @@ public interface ChannelDetailsView extends MvpView, FavoriteableView {
     void showMembers(List<User> users);
 
     @StateStrategyType(SkipStrategy.class)
-    void openEditChannel(Channel channel);
+    void openEditChannel(Channel channel, boolean invite);
 
     void showProgress(boolean show);
 
