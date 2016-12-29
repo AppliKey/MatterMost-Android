121d80459b46485d0d9707337f00670685d83c4c
Denis Nikiforov <denis.nikiforov@applikeysolutions.com>
2016-12-29 17:29:26 +0200

    Fix My message comes as unread

--- app/src/main/java/com/applikey/mattermost/App.java
+++ app/src/main/java/com/applikey/mattermost/App.java
@@ -14,6 +14,8 @@ import com.applikey.mattermost.manager.RxForeground;
 import com.applikey.mattermost.platform.socket.WebSocketService;
 import com.applikey.mattermost.utils.kissUtils.KissTools;
 import com.applikey.mattermost.web.images.ImageLoader;
+import com.arellomobile.mvp.MvpFacade;
+import com.arellomobile.mvp.PresentersCounter;
 import com.crashlytics.android.Crashlytics;
 import com.facebook.stetho.Stetho;
 import com.fuck_boilerplate.rx_paparazzo.RxPaparazzo;
@@ -81,6 +83,8 @@ public class App extends Application {
                         stopService(WebSocketService.getIntent(this));
                     }
                 }, Throwable::printStackTrace);
+
+        MvpFacade.getInstance().setPresentersCounter(new PresentersCounter());
     }
 
     @Override
