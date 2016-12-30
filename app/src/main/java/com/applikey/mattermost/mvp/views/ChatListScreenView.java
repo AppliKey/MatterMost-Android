dbb5eb6ffbd8d292a0ef652e6deb413f7f03c2ee
Denis Nikiforov <denis.nikiforov@applikeysolutions.com>
2016-12-30 16:30:41 +0200

    Fix empty screen

--- app/src/main/java/com/applikey/mattermost/mvp/views/ChatListScreenView.java
+++ app/src/main/java/com/applikey/mattermost/mvp/views/ChatListScreenView.java
@@ -4,25 +4,26 @@ import android.support.v4.app.Fragment;
 
 import com.applikey.mattermost.models.channel.Channel;
 import com.arellomobile.mvp.MvpView;
-import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy;
-import com.arellomobile.mvp.viewstate.strategy.SkipStrategy;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 import java.util.List;
 
-@StateStrategyType(value = SingleStateStrategy.class)
+@StateStrategyType(value = AddToEndSingleStrategy.class)
 public interface ChatListScreenView extends MvpView {
 
     void setToolbarTitle(String title);
 
-    @StateStrategyType(value = SkipStrategy.class)
+    @StateStrategyType(value = OneExecutionStateStrategy.class)
     void onChannelLoaded(Channel channel);
 
     /**
      * Platform-specific. We call it explicitly to separate this logic from logout.
      */
+    @StateStrategyType(value = OneExecutionStateStrategy.class)
     void stopWebSocketService();
 
-    @StateStrategyType(value = SkipStrategy.class)
+    @StateStrategyType(value = OneExecutionStateStrategy.class)
     void initViewPager(List<Fragment> pages);
 }
