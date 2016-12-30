f34ab67446611e8bef018b02e8df3dede280d979
Denis Nikiforov <denis.nikiforov@applikeysolutions.com>
2016-12-30 17:53:26 +0200

    Refactoring User profile

--- app/src/main/java/com/applikey/mattermost/mvp/views/UserProfileView.java
+++ app/src/main/java/com/applikey/mattermost/mvp/views/UserProfileView.java
@@ -3,15 +3,17 @@ package com.applikey.mattermost.mvp.views;
 import com.applikey.mattermost.models.channel.Channel;
 import com.applikey.mattermost.models.user.User;
 import com.arellomobile.mvp.MvpView;
-import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
-@StateStrategyType(value = SingleStateStrategy.class)
+@StateStrategyType(value = AddToEndSingleStrategy.class)
 public interface UserProfileView extends MvpView, FavoriteableView {
 
     void showBaseDetails(User user);
 
     void onMakeFavorite(boolean favorite);
 
+    @StateStrategyType(value = OneExecutionStateStrategy.class)
     void openDirectChannel(Channel channel);
 }
